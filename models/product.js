cff669d6618677ab520a35914041e46abb99fa58
mohitx1 <mohitrockx3@gmail.com>
2023-09-05 13:00:49 +0530

    minor modification of code

--- models/product.js
+++ models/product.js
@@ -1,38 +1,38 @@
 const fs=require('fs');
 const path=require('path')
 
+const p=path.join(path.dirname(process.mainModule.filename),
+'data',
+'products.json'
+);
+
+const getProductsFromFile=(cb)=>{
+
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]) ;
+        }else{
+            cb(JSON.parse(fileContent))//always returns list of objects
+        }
+
+    });
+}
+
 module.exports=class product{
     constructor(t){
         this.title=t;
     }
 
     save(){
-        const p=path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-        );
-        //must use arrow function otherwise this will not be able to access the data of class
-        fs.readFile(p,(err,fileContent)=>{
-            let products=[];
-            if(!err){
-                products=JSON.parse(fileContent);
-            }
+        getProductsFromFile(products=>{
             products.push(this);
             fs.writeFile(p,JSON.stringify(products),(err)=>{
                 console.log(err)
             });
-        })
+        });
+        //must use arrow function otherwise this will not be able to access the data of class
     }
     static fetchAll(cb){
-        const p=path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-        );
-        fs.readFile(p,(err,fileContent)=>{
-            if(err){
-                cb([]) 
-            }
-            cb(JSON.parse(fileContent))//always returns list of objects
-        });
+        getProductsFromFile(cb)
     }
 }
